063618941c93b0b5a7683bd6943fec008310df18
tamuno777 <davisbeulah07@gmail.com>
2023-08-10 07:20:02 -0700

    boots

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
 
-{
-  /* The following line can be included in your src/index.js or App.js file */
-}
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
